17f092eca19e70f508f64218dce75b2f88433bc6
qxz09bo <bhanu.akula@partner.bmwgroup.com>
2023-08-05 18:45:59 +0200

    intial changes

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <div class="top-section">
+      <h1>Welcome to EstimaPro</h1>
+      <p>Enter the User Story Number:</p>
+      <input v-model="userStory" type="text" placeholder="User Story Number">
+      <button @click="startSession">Start Session</button>
+      <user-list v-if="sessionStarted"></user-list>
+    </div>
+    <div class="bottom-section" v-if="sessionStarted">
+      <!-- Your results table will go here -->
+    </div>
+  </div>
+</template>
+
+<script>
+import UserList from './UserList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    UserList
+  },
+  data() {
+    return {
+      userStory: '',
+      sessionStarted: false
+    }
+  },
+  methods: {
+    startSession() {
+      if (this.userStory !== '') {
+        console.log(`Starting session for User Story: ${this.userStory}`)
+        this.sessionStarted = true
+      } else {
+        console.log('Please enter a User Story Number.')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  height: 100vh; /* Use the full viewport height */
+}
+
+.top-section {
+  flex: 0.6; /* 60% of the container's height */
+}
+
+.bottom-section {
+  flex: 0.4; /* 40% of the container's height */
+  overflow: auto; /* Add a scrollbar if the content is too tall */
+}
+</style>
---
+++ src/UserList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="user-list">
+      <div v-for="user in users" :key="user.id" class="user">
+        <img :src="user.image" :alt="user.name" class="user-image">
+        <p>{{ user.name }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'UserList',
+    data() {
+      return {
+        users: [
+          { id: 1, name: 'John Doe', image: 'https://via.placeholder.com/50' },
+          { id: 2, name: 'Jane Doe', image: 'https://via.placeholder.com/50' },
+          // Add more users as needed
+        ]
+      }
+    }
+  }
+  </script>
+  
+  <style>
+  .user-list {
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    padding: 20px;
+  }
+  
+  .user {
+    text-align: center;
+  }
+  
+  .user-image {
+    border-radius: 50%;
+  }
+  </style>
+  
