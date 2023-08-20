2b3ac78d473938f566010135e6b805398bb39386
qxz09bo <bhanu.akula@partner.bmwgroup.com>
2023-08-20 17:12:32 +0200

    user estimate  from another browser working

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,9 +1,22 @@
 <template>
   <div>
-    <h1>Enter your name</h1>
-    <input v-model="playerName" type="text" placeholder="Your name" />
-    <button @click="joinGame">Join</button>
-    <!-- Add card components or other UI elements here -->
+    <div v-if="!isJoined">
+      <h1>Enter your name</h1>
+      <input v-model="playerName" type="text" placeholder="Your name" />
+      <button @click="joinGame">Join</button>
+    </div>
+    <div v-if="isJoined">
+      <label for="estimate">Choose an estimate:</label>
+      <select id="estimate" v-model="selectedEstimate">
+        <option disabled value="">Please select an estimate</option>
+        <option>S</option>
+        <option>M</option>
+        <option>L</option>
+        <option>XL</option>
+        <option>?</option>
+      </select>
+      <button @click="submitEstimate" :disabled="!selectedEstimate">Estimate</button>
+    </div>
   </div>
 </template>
 
--- src/components/player.js
+++ src/components/player.js
@@ -5,25 +5,38 @@ import { v4 as uuidv4 } from 'uuid';
 export default {
     data() {
         return {
+            isJoined: false,
             socket: null,
             playerName: '',
+            selectedEstimate: '',
             roomName: this.$route.params.roomName, // Access the room name from the URL
         };
     },
     methods: {
         joinGame() {
-            // Code to join the room using this.roomName and this.playerName
-            console.log('Joining room:', this.roomName);
-            this.socket = io(`http://localhost:5000`, {
-                query: {
-                    room: this.roomName
-                }
-            });
+            if (!this.isJoined) {
+                // Code to join the room using this.roomName and this.playerName
+                console.log('Joining room:', this.roomName);
+                this.socket = io(`http://localhost:5000`, {
+                    query: {
+                        room: this.roomName
+                    }
+                });
 
-            this.socket.emit('join_room', {
-                'user': this.playerName,
-                'room': this.roomName
-            });
-        }
+                this.socket.emit('join_room', {
+                    user: this.playerName,
+                    room: this.roomName
+                });
+                this.isJoined = true;
+            }
+            else {
+                console.log('Already joined!');
+            }
+        },
+        submitEstimate() {
+            // Handle submitting the estimate
+            console.log('Estimated:', this.selectedEstimate);
+            this.socket.emit('user_estimation', { userid: this.socket.id, estimation: this.selectedEstimate })
+        },
     }
 }
